84e0a59f4f632f2a12f796c6ca950f8146ae7959
narin <narin45446@gmail.com>
2022-01-11 16:11:37 +0900

    모바일 확대 기능 수정

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<title>barlaw</title>
 	<meta name="description" content="">
 	<meta name="keywords" content="">
